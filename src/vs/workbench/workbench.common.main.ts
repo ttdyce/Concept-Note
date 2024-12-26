b9ff9156e986597e519e45a8013bed01bc459c1a
ttdyce <ttdyce@gmail.com>
2024-12-27 03:47:03 +0800

    Try disabling walkthrough and getting started feature

--- src/vs/workbench/workbench.common.main.ts
+++ src/vs/workbench/workbench.common.main.ts
@@ -327,8 +327,8 @@ import './contrib/surveys/browser/nps.contribution.js';
 import './contrib/surveys/browser/languageSurveys.contribution.js';
 
 // Welcome
-import './contrib/welcomeGettingStarted/browser/gettingStarted.contribution.js';
-import './contrib/welcomeWalkthrough/browser/walkThrough.contribution.js';
+// import './contrib/welcomeGettingStarted/browser/gettingStarted.contribution.js';
+// import './contrib/welcomeWalkthrough/browser/walkThrough.contribution.js';
 import './contrib/welcomeViews/common/viewsWelcome.contribution.js';
 import './contrib/welcomeViews/common/newFile.contribution.js';
 
